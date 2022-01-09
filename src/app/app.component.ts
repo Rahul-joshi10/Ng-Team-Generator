1b4997564fce210494c50ac7f9afe0ada131afbb
Rahul joshi <joshirahul329@gmail.com>
2022-01-09 16:51:31 +0530

    Team genrator project first commit- Add team memeber names functionality done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'team-generator';
+
+  newMemberName = "";
+  members: string[] = [];
+  errorMessage = "";
+
+  addMember() {
+    if (this.newMemberName && this.newMemberName !== '') {
+      this.members.push(this.newMemberName);
+      this.newMemberName = ""
+      this.errorMessage = ""
+    } else {
+      this.errorMessage = "Name can't be empty"
+    }
+
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
 }
