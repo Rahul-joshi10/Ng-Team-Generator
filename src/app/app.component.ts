9edf03a27dd32daf686c470356331d11df210bc5
Rahul joshi <joshirahul329@gmail.com>
2022-01-09 21:21:16 +0530

    Team generation core logic done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
   newMemberName = "";
   members: string[] = [];
   errorMessage = "";
+  numberOfTeams: number | "" = "";
+  teams: string[][] = [];
 
   addMember() {
     if (this.newMemberName && this.newMemberName !== '') {
@@ -32,5 +34,33 @@ export class AppComponent {
 
   onInput(member: string) {
     this.newMemberName = member;
+  }
+
+  getNumberOfTeams(value: string) {
+    this.numberOfTeams = Number(value);
+  }
+
+  generateTeams = () => {
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      return
+    }
+
+    const allMembers = [...this.members]
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) break;
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+        console.log(this.teams)
+      }
+    }
   }
 }
